f5ec792bb67650c9aa9fa66ce2462fdc8d7fffcf
nickbrowne <nickbrowne@users.noreply.github.com>
2018-09-27 16:45:03 +1000

    Basic event structure

---
+++ app/event/base.rb
@@ -0,0 +1,7 @@
+module Event
+  class Base
+    def apply(aggregate)
+      raise "Not implemented"
+    end
+  end
+end
---
+++ app/event/user/registered.rb
@@ -0,0 +1,9 @@
+module Event
+  module User
+    class Registered < Event::Base
+      def apply(aggregate)
+
+      end
+    end
+  end
+end
