3aeccd8f6d54c9b0fd793048b70a72db6b62d954
Tiago Amaro <tiagopadrela@gmail.com>
2018-09-28 17:57:58 -0300

    Update user schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,13 @@ ActiveRecord::Schema.define(version: 2018_09_27_081258) do
   enable_extension "plpgsql"
 
   create_table "user_events", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.string "type", null: false
     t.jsonb "data", null: false
+    t.jsonb "metadata", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
