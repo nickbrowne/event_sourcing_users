499a87d52d2c1294f1b2c765b51bb8fa82ec5107
Tiago Amaro <tiagopadrela@gmail.com>
2018-10-02 18:03:28 -0300

    Do not use STI for events

--- db/migrate/20180927081258_add_user_events.rb
+++ db/migrate/20180927081258_add_user_events.rb
@@ -1,8 +1,6 @@
 class AddUserEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :user_events do |t|
-      t.string :type, null: false
-
       t.jsonb :data, null: false
       t.jsonb :metadata, null: false
 
